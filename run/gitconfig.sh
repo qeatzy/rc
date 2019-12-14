50c75afebac417a34ba84ac91487f7ef5ad201e2
U-DESKTOP-IPPPB9D\zyq3e <qeatzy@users.noreply.github.com>
2019-12-14 09:29:51 +0800

    shell, add complete for pe

--- run/gitconfig.sh
+++ run/gitconfig.sh
@@ -4,4 +4,5 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 
-git config --global core.pager 'vim -c "sil! keepp %sm/\e.\{-}m//ge" -c 1 -'
+git config --global core.pager 'vim --not-a-term -c "sil! keepp %sm/\e.\{-}m//ge" -c 1 -'
+git config --global core.editor vim
